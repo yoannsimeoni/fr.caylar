3cc22336603e6ab8cdfd4f18e0e3c050ffa9d186
Yoann <yoann.simeoni@gmail.com>
2016-10-15 19:32:34 +0200

    Revert "Test for Ced"
    This reverts commit 02d441fa0af6223a7e339958ba6e061bd2b0d147.

--- fr.caylar.devices/src/fr/caylar/devices/api/package-info.java
+++ fr.caylar.devices/src/fr/caylar/devices/api/package-info.java
@@ -1,4 +1,4 @@
 /**
- * This package contains the interfaces and utility class that you should use. hi
+ * This package contains the interfaces and utility class that you should use.
  */
 package fr.caylar.devices.api;
